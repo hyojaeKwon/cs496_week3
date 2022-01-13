34699f7a4ed8fe41a9ab61d48ae44dd3701febe0
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-01-13 16:44:22 +0900

    merged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import FirstComponent from "./component/FirstComponent";
 import './App.css';
+import Header from './component/common/header';
+import Navigation from './component/common/navigation';
 
 function App() {
   return (
     <div className="App">
         <FirstComponent/>
+      <Header/>
+      <Navigation/>
     </div>
   );
 }
---
+++ src/component/common/navigation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = styled.div`
+    display: inline-block;
+    vertical-align: top;
+    width: 80rem;
+    margin-top: 2%;
+    padding-bottom: 0.5%;
+    border-bottom: 1px solid #e3e3e3;
+`
+const Nav = styled.div`
+    width: 6rem;
+    height: 2rem;
+    margin-left: 3%;
+    text-align: center;
+    display: inline-block;
+    font-size: 1rem;
+    font-weight: 600;
+    color: #686D76;
+    text-decoration: none;
+
+    &:hover{
+        font-weight: 800;
+        color: #4F8A8B;
+    }
+`
+
+const Navigation = () => {
+    return(
+        <NavBar>
+            <Nav>리트래블</Nav>
+            <Nav>여행 계획</Nav>
+            <Nav>나의 여행 지도</Nav>
+            <Nav>여행 둘러보기</Nav>
+            <div style={{ width: '35%', display: 'inline-block'}}></div>
+            <Nav>마이페이지</Nav>
+        </NavBar>
+    )
+}
+
+export default Navigation;
