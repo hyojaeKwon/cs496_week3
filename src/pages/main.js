e00b66e85cee67b87d558fddcabd3bcab4789e69
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-01-13 19:51:02 +0900

    create main file

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../component/common/header';
 import Navigation from '../component/common/navigation';
+import FirstComponent from '../component/FirstComponent';
+import TrendingPost from "../component/TrendingPost";
 
 const Main = () => {
     return(
         <div>
             <Header/>
             <Navigation/>
+            <FirstComponent/>
+            <TrendingPost/>
         </div>
     );
 };
