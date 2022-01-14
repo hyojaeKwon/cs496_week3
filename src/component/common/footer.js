81c07ba82ad978f2370c403a506af9f3648acb43
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-01-14 22:14:14 +0900

    foote\r

--- src/component/common/footer.js
+++ src/component/common/footer.js
@@ -1,15 +1,51 @@
 import react from "react";
 import styled from "styled-components";
 
+
+const Wrapper = styled.div`
+  width : 100%;
+  background-color: rgba(234, 234, 234, 0.3);
+  border-top: 1px solid #E9ECEF;
+`;
+
 const FooterContainer = styled.div`
-  height: 125px;
+  max-height: 125px;
   width: 1060px;
-  border-top: 1px solid #000;
+  margin : 0 auto;
+  margin-top: 10px;
+  display: grid;
+  margin-bottom: 40px;
+  grid-template-rows: 1fr 1fr;
+  grid-auto-flow: column;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  margin-top: 30px;
+`;
+
+const Text = styled.span`
+  display: inline-block;
+  margin-right: 20px;
 `;
 
 const Footer = () => {
   return (
-    <FooterContainer/>
+    <Wrapper>
+      <FooterContainer>
+      <TextContainer>
+        <Text>
+          이용 약관
+        </Text>
+        <Text>
+          개인 정보 처리 방침
+        </Text>
+      </TextContainer>
+      <TextContainer>
+        <Text>2022 Mad Camp Week 3 | 이우진 최윤지 권효재</Text>
+      </TextContainer>
+      </FooterContainer>
+    </Wrapper>
   );
 };
 
