eab7c66c45c7fe48d2bd7782a02ba02b4be8a2e0
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-01-18 04:50:36 +0900

    add map

--- src/component/plan/routemap.js
+++ src/component/plan/routemap.js
@@ -2,46 +2,67 @@ import React, { useEffect } from 'react';
 
 const { kakao } = window;
 
-const RouteMap = ({location}) => {
+const RouteMap = (locArray) => {
+    const locArr = locArray['props'];
     
-    // console.log(files);
+    // console.log(locArr[0]['x']);
     useEffect(() => {
-        const average = ( arr ) => {
+        var avgX = 0;
+        var avgY = 0;
+        const averageX = (arr) => {
             var sum = 0;
-
-            for(let i = 0; i < arr.length; i++) {
-                sum += arr[i];
+            for(let i = 0 ; i < arr.length; i++){
+                sum += Number(arr[i]['x']);
             }
+            // console.log(sum) 
+            return sum/arr.length;
+        };
 
-            return sum / arr.length;
+        const averageY = (arr) => {
+            var sum = 0;
+            for(let i = 0 ; i < arr.length; i++){
+                sum += Number(arr[i]['y']);
+            }
+            return sum/arr.length;
         }
-        const Lat = new Array(36.36384436862, 36.3704985792808, 36.37601682770846);
-        const Lng = new Array(127.343250512653, 127.344834223158, 127.36296203815101);
 
         const mapContainer = document.getElementById('map');
         const mapOption = {
-            center: new kakao.maps.LatLng(average(Lat), average(Lng)),
-            level: 5,
+            center: new kakao.maps.LatLng(averageY(locArr),averageX(locArr)),
+            level: 8,
             mapTypeId: kakao.maps.MapTypeId.ROADMAP
         };
-
-        const map = new kakao.maps.Map(mapContainer, mapOption);
-
-        const marker1 = new kakao.maps.Marker({
-            position: new kakao.maps.LatLng(36.3621376213944, 127.350057490123),
-            map: map
-        })
-        const marker2 = new kakao.maps.Marker({
-            position: new kakao.maps.LatLng(36.3610860008778, 127.344771523599),
-            map: map,
-        })
+        // const map = new kakao.maps.Map();
+        console.log(averageX(locArr))
+        const map = new kakao.maps.Map(mapContainer,  mapOption)
+        console.log("Hi")
+        const pathList = [];
+        const markers = (locArr) => {
+            console.log("ok")
+            for(let i = 0 ; i < locArr.length; i++){
+                pathList.push(new kakao.maps.LatLng(Number(locArr[i]['y']),Number(locArr[i]['x'])));
+                const marker = new kakao.maps.Marker({
+                    position: new kakao.maps.LatLng(Number(locArr[i]['y']),Number(locArr[i]['x'])),
+                    map: map
+                })
+            }
+            console.log(pathList)
+        }
+        markers(locArr);
+        // console.log(pat)
+        // const marker1 = new kakao.maps.Marker({
+        //     position: new kakao.maps.LatLng(36.3621376213944, 127.350057490123),
+        //     map: map
+        // })
+        // const marker2 = new kakao.maps.Marker({
+        //     position: new kakao.maps.LatLng(36.3610860008778, 127.344771523599),
+        //     map: map,
+        // })
 
         const polyline = new kakao.maps.Polyline({
             map: map,
             path: [
-                new kakao.maps.LatLng(36.36384436862, 127.343250512653),
-                new kakao.maps.LatLng(36.3704985792808, 127.344834223158),
-                new kakao.maps.LatLng(36.37601682770846, 127.36296203815101),
+               pathList
             ],
             strokeWeight: 3,
             strokecolor: '#FF000',
@@ -51,7 +72,7 @@ const RouteMap = ({location}) => {
     }, [])
 
     return(
-        <div id="map" style={{width: '700px', height: '700px'}}></div>
+        <div id="map" style={{'width': '700px', 'height': '700px'}}></div>
     )
 }
 
--- src/pages/plan.js
+++ src/pages/plan.js
@@ -3,18 +3,107 @@ import Header from '../component/common/header';
 import Navigation from '../component/common/navigation';
 import Footer from '../component/common/footer';
 import RouteMap from '../component/plan/routemap';
+import styled from 'styled-components';
+
 
 const Plan = ({location}) => {
-    const files = location.state.files;
-    console.log(files);
-    return(
-        <div>
-            <Header/>
-            <Navigation/>
-            <RouteMap/>
-            <Footer/>
-        </div>
-    )
+	const files = location.state.files;
+	console.log(files);
+	return(
+		<div>
+			<Header/>
+			<Navigation/>
+			<div style={{"display":"flex","justify-content":"center"}}>
+				<Container>
+				<TitleBar>
+					<div style={{"display":"flex","alignItems":"flex-end"}}>
+						<Title>
+							여행 계획 추천
+						</Title>
+						<SubTitle>
+							여행 계획 세우기
+						</SubTitle>
+					</div>
+					<div>
+						<Button>
+							추천 여행지
+						</Button>
+					</div>
+				</TitleBar>
+				<div style={{'display':'flex','width':'100%','justifyContent':'space-between'}}>
+					<RouteMap
+					props={files}/>
+					<RecoContainer>
+
+					</RecoContainer>
+				</div>
+
+				</Container>
+			</div>
+			<Footer/>
+		</div>
+	)
 };
+const RecoContainer = styled.div`
+	height: 100%;
+	width: 330px;
+	display: flex;
+	border: 1px solid #000;
+	flex-direction: column;
+`;
+
+const Button = styled.button`
+	border: 0.5px solid #4F8A8B;
+  width: 122px;
+  color: #fff;
+  /* background-color: #4F8A8B; */
+  height: 40px;
+  background-color: #4F8A8B;
+  border-radius: 3px;
+  font-size: 13px;
+  line-height: 15px;
+  text-decoration: none;
+  text-align: center;
+  display: inline-block;
+  align-items: center;
+  justify-content: center;
+  &:hover{
+    color: #4F8A8B;
+    background-color: #fff;
+    border: 0.5px solid #4F8A8B;
+  }
+`;
+
+const Title = styled.div`
+	font-weight: 600;
+	font-size: 36px;
+	margin-left: 10px;
+	line-height: 42px;
+`;
+
+const SubTitle = styled.div`
+	font-weight: 600;
+	font-size: 28px;
+	line-height: 33px;
+	margin-left: 10px;
+	color: #848484;
+`;
+
+const TitleBar = styled.div`
+	height: 70px;
+	width: 100%;
+	display: flex;
+	margin-top: 20px;
+	padding-bottom: 20px;
+	align-items: flex-end;
+	margin-bottom: 40px;
+	border-bottom: 0.5px solid #e3e3e3;
+	justify-content: space-between;
+
+`
+const Container = styled.div`
+	width: 1060px;
+	height: auto;
+`;
 
 export default Plan;
